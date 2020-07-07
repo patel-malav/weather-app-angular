8c7c4ad89e84cd00ebf4d26a8d1975cbcc1bfc8d
patel-malav <patelmalav64@gmail.com>
2020-07-08 04:15:43 +0530

    🛠 Api Correction for Prodcution build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   openweatherApiKey: '<Your-Secret-App-Key>',
-  openweatherApi: 'https://api.openweathermap.org/data/2.5/weather',
+  openweatherApi: 'https://api.openweathermap.org/data/2.5/',
 };

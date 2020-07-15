10b38ce3d7d374ad7a25312341d7e2d346ac9266
patel-malav <patelmalav64@gmail.com>
2020-07-15 14:15:53 +0530

    👿 Spelling Mistake

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  openweatherApiKeyL: '8d1daa20188bbfba252ac88c15247a73',
+  openweatherApiKey: '8d1daa20188bbfba252ac88c15247a73',
   // openweatherApiKey: '<Your-Secret-App-Key>',
   openweatherApi: 'https://api.openweathermap.org/data/2.5/',
 };
